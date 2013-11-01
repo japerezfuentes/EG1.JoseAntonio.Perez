d2acd5c0e82e2f714e01a77564d86d1d245b0791
japerezfuentes <japerezfuentes@gmail.com>
2013-11-01 02:56:35 +0100

    CD2

--- src/operaciones/Operaciones.java
+++ src/operaciones/Operaciones.java
@@ -1,12 +1,14 @@
 package operaciones;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Operaciones {
 
 	public static void main (String args[]) throws IOException, Exception
 	{
-		
+		InputStreamReader isr = new InputStreamReader(System.in);
+
 		
 	}
 }
